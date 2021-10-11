087531a7e3a9710f22aa2e56fb54904e37d6e289
unknown <mohtashim@rightsolutions4u.com>
2021-10-11 05:43:38 +0500

    Register form

--- StorePromotion/StorePromotion.UI/Controllers/StoreOwnerController.cs
+++ StorePromotion/StorePromotion.UI/Controllers/StoreOwnerController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StorePromotion.Common.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace StorePromotion.UI.Controllers
@@ -29,11 +33,32 @@ namespace StorePromotion.UI.Controllers
 
         // POST: StoreOwnerController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult<StoreOwner>> Create(IFormCollection collection)
         {
             try
             {
+
+                StoreOwner storeOwner = new StoreOwner
+                {
+                    Fname = collection["FName"],
+                    Lname = collection["LName"],
+                    CellNo = collection["CellNo"],
+                    Email = collection["Email"],
+                    UserId = collection["UserId"],
+                    Pwd = collection["Pwd"],
+                    IsActive = true
+                };
+                string output = JsonConvert.SerializeObject(storeOwner);
+                var data = new StringContent(output, Encoding.UTF8, "application/json");
+                var url = "https://localhost:44356/api/StoreOwner";
+                /*var url = "https://localhost:44356/api/Addresses";*/
+
+                var client = new HttpClient();
+                var response = await client.PostAsync(url, data);
+                var StoreOwner = response.Content.ReadAsStringAsync().Result;
+                var StoreOwner1 = JsonConvert.DeserializeObject<StoreOwner>(StoreOwner);
+                var StoreOwnerName = StoreOwner1.Fname;
+                ViewBag.Name = StoreOwnerName;
                 return RedirectToAction(nameof(Index));
             }
             catch
